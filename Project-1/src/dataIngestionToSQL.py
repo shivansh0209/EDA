e29f263745882cc2b9f6c1806aafa3a416df3be1
Shivansh Pandey <shivanshpandey27@gmail.com>
2026-06-04 12:26:52 +0530

    Project-1: Updated plana and dataIngestion Logic

--- Project-1/src/dataIngestionToSQL.py
+++ Project-1/src/dataIngestionToSQL.py
@@ -1,13 +1,22 @@
+import os
 import pandas as pd
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from urllib.parse import quote_plus
 
-# reads tabular data from a CSV file and converts into dataframe
-df = pd.read_csv("employees.csv")
+load_dotenv()
 
+user = os.getenv("DB_USER")
+password = quote_plus(os.getenv("DB_PASSWORD"))
+host = os.getenv("DB_HOST")
+database = os.getenv("DB_NAME")
 
 engine = create_engine(
-    "mysql+pymysql://username:password@localhost/company_db"
+    f"mysql+pymysql://{user}:{password}@{host}/{database}"
 )
+
+df = pd.read_csv("employees.csv")
 
 df.to_sql(
     "employees",
